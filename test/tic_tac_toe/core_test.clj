1c1ac9ee6445a4a2974e0796af6f940a592d8abf
martin hablak <martin.hablak@sk.ibm.com>
2021-03-29 16:53:22 +0200

    red: board elements should contain offset property

--- test/tic_tac_toe/core_test.clj
+++ test/tic_tac_toe/core_test.clj
@@ -5,6 +5,10 @@
 (deftest board-test
   (testing "describe board"
     (testing "empty board of size 3x3 should contain 9 slots"
-
       (is (= 9 (->> (empty-board 3)
-                    (count)))))))
+                    (count)))))
+    (testing "all board elements should contain offset property"
+      (is (= 9
+             (->> (empty-board 3)
+                  (filter (fn [e] (:offset e)))
+                  (count)))))))
