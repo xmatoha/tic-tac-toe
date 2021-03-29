fd81c8c42586a6eec2b254d588e7d8ecb2de4e28
martin hablak <martin.hablak@sk.ibm.com>
2021-03-29 17:17:59 +0200

    pomodoro finished commit

--- src/tic_tac_toe/core.clj
+++ src/tic_tac_toe/core.clj
@@ -3,6 +3,11 @@
 (defn empty-board [board-size]
   (->>
    (range (* board-size board-size))
-   (map (fn [idx] {:offset idx :state :e}))))
+   (map (fn [idx] {:offset idx :state :e}))
+   (into [])))
 
-(defn occupy [board row column who])
+(defn occupy [board row column who]
+
+  (subvec board (+ (* 3 (- row 1)  (- column 1))) 1))
+
+(occupy (empty-board 3) 1 1 :x)
