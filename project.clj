7fa8512e9b2bb7f1c8dbc9060f49e6062d8e66fc
Martin Hablak <martin.hablak@sk.ibm.com>
2022-04-05 16:27:24 +0200

    feat: configured tests and linting

--- project.clj
+++ project.clj
@@ -3,7 +3,15 @@
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.0"]]
+  :dependencies [[org.clojure/clojure "1.10.3"]
+                 [clj-kondo "RELEASE"]]
   :main ^:skip-aot tic-tac-toe.core
   :target-path "target/%s"
-  :profiles {:uberjar {:aot :all}})
+  :aliases {"test" ["with-profile" "+kaocha" "run" "-m" "kaocha.runner"]
+            "watch" ["with-profile" "+kaocha" "run" "-m" "kaocha.runner" "--watch"]
+            "lint" ["run" "-m" "clj-kondo.main" "--lint" "src"]}
+  :profiles {:kaocha {:dependencies [[lambdaisland/kaocha-cucumber "0.0-53"]
+                                     [lambdaisland/kaocha "1.64.1010"]
+                                     [lambdaisland/kaocha-cloverage "1.0.75"]]}
+             :uberjar {:aot :all}})
+
