0992fc2f6a74072a8be64079e81128df8747097b
Martin Hablak <martin.hablak@sk.ibm.com>
2022-04-12 13:50:22 +0200

    chore: ci

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject tic-tac-toe "0.1.0-SNAPSHOT"
+(defproject tic-tac-toe :project/status-to-version
   :description "tic tac toe kata"
   :dependencies [[org.clojure/clojure "1.10.3"]
                  [clj-kondo "2022.03.09"]
@@ -14,7 +14,21 @@
             "watch" ["with-profile" "+kaocha" "run" "-m" "kaocha.runner" "--fail-fast" "--watch" "unit"]
             "lint" ["run" "-m" "clj-kondo.main" "--lint" "src"]}
   :plugins [[refactor-nrepl "3.5.2"]
-          [cider/cider-nrepl "0.28.3"]]
+            [cider/cider-nrepl "0.28.3"]
+            [me.arrdem/lein-git-version "2.0.3"]]
+
+  :git-version
+  {:status-to-version
+   (fn [{:keys [tag version branch ahead ahead? dirty?] :as git}]
+     (assert (re-find #"\d+\.\d+\.\d+" tag)
+             "Tag is assumed to be a raw SemVer version")
+     (if (and tag (not ahead?) (not dirty?))
+       tag
+       (let [[_ prefix patch] (re-find #"(\d+\.\d+)\.(\d+)" tag)
+             patch            (Long/parseLong patch)
+             patch+           (inc patch)]
+         (format "%s.%d" prefix patch+))))}
+
   :profiles {:kaocha {:dependencies [[lambdaisland/kaocha-cucumber "0.0-53"]
                                      [lambdaisland/kaocha "1.64.1010"]
                                      [lambdaisland/kaocha-cloverage "1.0.75"]]}
